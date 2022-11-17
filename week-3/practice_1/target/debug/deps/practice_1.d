dfd89a679ab57f2ebcf6a362b9db1b8ade26bfb5
nwaizu <115726931+nwaizu@users.noreply.github.com>
2022-11-17 16:23:21 +0100

    Delete week-3/practice_1/target directory

--- week-3/practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\izuch\OneDrive\Documents\i_nwachukwuCSC101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\izuch\OneDrive\Documents\i_nwachukwuCSC101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
