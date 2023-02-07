b80b793a588dfd8258538fc04d6ed72d05c51786
nwaizu <115726931+nwaizu@users.noreply.github.com>
2023-02-07 12:33:11 +0100

    Add files via upload

---
+++ week-14/project_1/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\P COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\target\debug\deps\project_1.exe: src\main.rs
+
+C:\Users\P COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\target\debug\deps\project_1.d: src\main.rs
+
+src\main.rs:
---
+++ week-14/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\P\ COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\target\debug\project_1.exe: C:\Users\P\ COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\src\main.rs
