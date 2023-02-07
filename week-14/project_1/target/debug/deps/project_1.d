82476d535e64b64703adb085e44508e71bcea627
nwaizu <115726931+nwaizu@users.noreply.github.com>
2023-02-07 12:34:19 +0100

    Delete week-14/project_1 directory

--- week-14/project_1/target/debug/deps/project_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\P COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\target\debug\deps\project_1.exe: src\main.rs
-
-C:\Users\P COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\target\debug\deps\project_1.d: src\main.rs
-
-src\main.rs:
--- week-14/project_1/target/debug/project_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\P\ COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\target\debug\project_1.exe: C:\Users\P\ COMPUTERS\Documents\i_nwachukwuCSC101-main\week-14\project_1\src\main.rs
